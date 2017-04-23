8583c292e39d7c356ac6de5b0489ac497f783532
SeoYH1017 <yunho101775@gmal.com>
2017-04-23 23:49:11 +0900

    NodeJS

---
+++ Ex_06_nodeJS.js
@@ -0,0 +1,15 @@
+var age=20;
+console.log('나이 : %d',age);
+
+var name='소녀시대';
+console.log('이름 : %s',name);
+
+var Person={};
+
+Person['age']=20;
+Person['name']='소녀시대';
+Person.mobile='010-1000-1000';
+
+console.log('나이 : %d',Person.age);
+console.log('이름 : %s',Person.name);
+console.log('전화 : %s',Person['mobile']);
